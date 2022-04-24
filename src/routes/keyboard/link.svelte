32adabba326728fe3b771f365ee45ec256c1b830
takashiski <takashiskibb@gmail.com>
2022-04-24 21:42:25 +0900

    たまに使うけどググってぱっと見つからないやつまとめた

---
+++ src/routes/keyboard/link.svelte
@@ -0,0 +1,15 @@
+<h1>頻出リンク集</h1>
+
+
+<p><a href="https://sekigon-gonnoc.github.io/promicro-web-updater/index.html">Pro Micro Web Updater</a></p>
+<p><a href="https://www.caniusevia.com/">VIA</a></p>
+<p><a href="https://remap-keys.app/">remap</a></p>
+<p><a href="https://github.com/ijprest/kle-serial">kle-serial(repo)</a></p>
+<p><a href="https://github.com/ijprest/keyboard-layout-editor">keyboard-layout-editor(repo)</a></p>
+<p><a href="http://www.keyboard-layout-editor.com/">keyboard-layout-editor.com</a></p>
+<p><a href="https://qmk.fm/converter/">Convert KLE raw to QMK info.json</a></p>
+<p><a href="https://github.com/qmk/qmk_firmware/blob/master/docs/reference_info_json.md">info.json format</a></p>
+<p><a href="https://ek-switch-hitter.software.informer.com/">EK Switch Hitter(unofficial mirror)</a></p>
+<p><a href="https://ht-deko.com/arduino/usbasp.html">USBaspの日本語解説記事</a></p>
+<p><a href="https://docs.qmk.fm/#/">QMK documentation</a></p>
+<p><a href="https://docs.qmk.fm/#/ja/">QMKドキュメント</a></p>
